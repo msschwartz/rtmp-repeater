ca715610ea9a9b35c8a7a7b7317735a81c9e7ceb
Michael Schwartz <michael@slytrunk.com>
2020-07-30 07:22:36 -0400

    Update configs

--- src/config.js
+++ src/config.js
@@ -1,12 +1,17 @@
 module.exports = {
     arabic: {
-        primary: 'rtmp://rtmp.abnsat.com/ingest/live',
+        primary: 'rtmp://rtmp.abnsat.com/ingest/arabiclive',
         backup: 'rtmp://rtmp.abnsat.com/ingest/v1zos2fgbhrir289',
-        destination: 'rtmp://rtmp.abnsat.com/ingest/arabic',
+        destination: 'rtmp://localhost/live/arabic',
+    },
+    abnsama: {
+        primary: 'rtmp://rtmp.abnsat.com/ingest/abnsamalive',
+        backup: 'rtmp://rtmp.abnsat.com/ingest/wxpkr2rgbfjqvda9',
+        destination: 'rtmp://localhost/live/abnsama',
     },
     trinity: {
-        primary: 'rtmp://rtmp.abnsat.com/ingest/live',
+        primary: 'rtmp://rtmp.abnsat.com/ingest/trinitylive',
         backup: 'rtmp://rtmp.abnsat.com/ingest/zehkcgjtbdblut09',
-        destination: 'rtmp://rtmp.abnsat.com/ingest/trinity',
+        destination: 'rtmp://localhost/live/trinity',
     },
 };
